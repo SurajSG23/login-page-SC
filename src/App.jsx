6954df161cf5832abffab89ee8857cf177dbed7d
Suraj S G Dhanva <surajsgd23@gmail.com>
2024-11-28 06:11:36 +0530

    login page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import './App.css'
+import  Body  from './Components/Body'
+function App() {
+  return (
+    <>
+      <Body/>
+    </>
+  )
+}
+
+export default App
